63405637b62e852a04c4bdd2bf8a103c5e753462
Michael Kestner <mi.kestner@outlook.com>
2026-05-17 22:32:52 -0400

    Started adding functions for the config reader

--- src/configs.rs
+++ src/configs.rs
@@ -1,9 +1,25 @@
-// use std::env;
-// use std::path;
+// Copyright (c) 2026 Michael Kestner. All Rights Reserved.
+// configs.rs
+#![allow(unused)]
+use std::env;
+use std::io;
+use std::path;
 
 use shellrs::get_home_directory;
 
-pub fn _get_config_path() -> String {
-    let _home_dir = get_home_directory();
-    String::from("Hello")
+pub fn get_config_path() -> io::Result<String> {
+    let home_dir = get_home_directory()?;
+    let config_path = "/.config/warlock-shell/warlock.conf";
+
+    Ok(home_dir + config_path)
 }
+
+fn config_file_exists() -> bool {
+    if let Ok(config_path) = get_config_path() {
+        path::Path::new(&config_path).exists()
+    } else {
+        false
+    }
+}
+
+pub fn create_config_file() {}
